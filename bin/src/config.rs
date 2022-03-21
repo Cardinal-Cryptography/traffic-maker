5e37f0a4c9eb2463c01c374a92e91a9073d4aa73
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-03-21 07:58:55 +0100

    AZ-650: Expose stats from backend (#3)

--- bin/src/config.rs
+++ bin/src/config.rs
@@ -6,4 +6,8 @@ pub struct Config {
     /// WS endpoint address of the node to connect to
     #[clap(long, default_value = "127.0.0.1:9944")]
     pub node: String,
+
+    /// Where to expose stats
+    #[structopt(long, default_value = "127.0.0.1:8080")]
+    pub expose_host: String,
 }
