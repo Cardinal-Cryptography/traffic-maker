28778757df4e09204656db149d753f11a6ef557a
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-04-06 13:46:03 +0200

    big brain time

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -3,6 +3,14 @@ set -euo pipefail
 
 STATS_BASE_URL=${STATS_BASE_URL:-"http://127.0.0.1:8080"}
 
+# Unpack frontend if applicable
+FRONTEND_PACK="frontend.tar.gz"
+if [ -f "$FRONTEND_PACK" ]; then
+  tar -xzvf monitoring.tar.gz
+fi
+
+pushd monitoring
 RUST_LOG=warn trunk serve --release &
+popd
 
 RUST_LOG=warn backend
