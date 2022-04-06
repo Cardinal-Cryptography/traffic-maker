006adcaf05bc1375623cd574aaf3bb16edd49ac8
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-04-06 14:35:12 +0200

    typo

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -6,7 +6,7 @@ STATS_BASE_URL=${STATS_BASE_URL:-"http://127.0.0.1:8080"}
 # Unpack frontend if applicable
 FRONTEND_PACK="frontend.tar.gz"
 if [ -f "$FRONTEND_PACK" ]; then
-  tar -xzvf monitoring.tar.gz
+  tar -xzvf "$FRONTEND_PACK"
 fi
 
 pushd monitoring
