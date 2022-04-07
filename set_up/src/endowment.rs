1821f4c73683a52ec8486e43ce23a78bc218e51c
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-04-07 15:10:01 +0200

    Batch endowing (#14)

--- set_up/src/endowment.rs
+++ set_up/src/endowment.rs
@@ -12,25 +12,54 @@ use chain_support::keypair_derived_from_seed;
 use crate::{lib::real_amount, CliConfig};
 
 #[derive(Clone, Debug, Deserialize)]
-pub struct Endowment {
-    pub amount: u64,
-    pub accounts: Vec<String>,
+pub struct Account {
+    pub name: String,
+    pub copies: Option<usize>,
 }
 
-fn set_endowment(sudo_connection: &Connection, account: AccountId, amount: u128) {
-    // Unfortunately, `Connection::balance_set_balance` does not work. Calling it without wrapping
-    // with `Sudo::sudo` results in `BadOrigin`, so the inner extrinsic must be created with
-    // `compose_call` instead of `compose_extrinsic` as it is done in `Connection::balance_set_balance`.
-    let xt = compose_call!(
-        sudo_connection.metadata,
-        "Balances",
-        "set_balance",
-        GenericAddress::Id(account),
-        Compact(amount), // free balance
-        Compact(0u128)   // reserved balance
-    );
-    let xt = compose_extrinsic!(sudo_connection, "Sudo", "sudo", xt);
+#[derive(Clone, Debug, Deserialize)]
+pub struct Endowment {
+    pub amount: u64,
+    pub accounts: Vec<Account>,
+}
+
+type SudoConnection = Connection;
+
+fn batch_set_endowment(sudo_connection: &SudoConnection, accounts: Vec<AccountId>, amount: u128) {
+    let xts = accounts
+        .iter()
+        .map(|account| {
+            let endowment_call = compose_call!(
+                sudo_connection.metadata,
+                "Balances",
+                "set_balance",
+                GenericAddress::Id(account.clone()),
+                Compact(amount), // free balance
+                Compact(0u128)   // reserved balance
+            );
+            compose_call!(sudo_connection.metadata, "Sudo", "sudo", endowment_call)
+        })
+        .collect::<Vec<_>>();
+    let xt = compose_extrinsic!(sudo_connection, "Utility", "batch", xts);
     send_xt(sudo_connection, xt, Some("Set endowment"), Finalized);
+}
+
+fn flatten_accounts(accounts: &[Account]) -> Vec<AccountId> {
+    accounts
+        .iter()
+        .flat_map(|a| {
+            if a.copies.is_none() {
+                vec![a.name.clone()]
+            } else {
+                (0..a.copies.unwrap())
+                    .map(|i| format!("{}{}", a.name, i))
+                    .collect()
+            }
+        })
+        .map(keypair_derived_from_seed)
+        .map(|kp| kp.public())
+        .map(AccountId::from)
+        .collect()
 }
 
 pub fn perform_endowments(cli_config: &CliConfig, endowments: &[Endowment]) {
@@ -38,19 +67,12 @@ pub fn perform_endowments(cli_config: &CliConfig, endowments: &[Endowment]) {
     let sudo_connection = create_connection(&cli_config.node).set_signer(sudo);
 
     for Endowment { amount, accounts } in endowments {
-        let accounts = accounts
-            .iter()
-            .map(keypair_derived_from_seed)
-            .map(|kp| kp.public())
-            .map(AccountId::from)
-            .collect();
+        let accounts = flatten_accounts(accounts);
 
         if cli_config.transfer {
             balances_batch_transfer(&sudo_connection, accounts, real_amount(amount));
         } else {
-            for account in accounts {
-                set_endowment(&sudo_connection, account, real_amount(amount))
-            }
+            batch_set_endowment(&sudo_connection, accounts, real_amount(amount));
         }
     }
 }
