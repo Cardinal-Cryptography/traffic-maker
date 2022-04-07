6d1a6857971398cc3361484e2d0ad6316d2f5f3d
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-04-07 11:53:43 +0200

    review

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -3,12 +3,6 @@ set -euo pipefail
 
 STATS_BASE_URL=${STATS_BASE_URL:-"http://127.0.0.1:8080"}
 
-# Unpack frontend if applicable
-FRONTEND_PACK="frontend.tar.gz"
-if [ -f "$FRONTEND_PACK" ]; then
-  tar -xzvf "$FRONTEND_PACK"
-fi
-
 pushd monitoring
 RUST_LOG=warn trunk serve --release &
 popd
