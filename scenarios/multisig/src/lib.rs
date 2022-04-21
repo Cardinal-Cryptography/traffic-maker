8914cbe7e80eb8d108514ac15c9d6a89774e4428
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-04-21 18:43:36 +0200

    checkpoint

--- scenarios/multisig/src/lib.rs
+++ scenarios/multisig/src/lib.rs
@@ -1,1 +1,105 @@
+mod multisig;
+use anyhow::Result as AnyResult;
+use rand::{random, thread_rng, Rng};
+use serde::Deserialize;
+use thiserror::Error;
 
+/// Gathers all possible errors from this module.
+#[derive(Debug, Error)]
+pub enum MultisigError {
+    #[error("👪❌ Provided threshold value is too high.")]
+    ThresholdTooHigh,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub enum PartySize {
+    Small,
+    Medium,
+    Large,
+    Precise(usize),
+}
+
+impl PartySize {
+    pub fn get(self, upper_bound: usize) -> usize {
+        match self {
+            PartySize::Small => thread_rng().gen_range(2..6),
+            PartySize::Medium => thread_rng().gen_range(6..15),
+            PartySize::Large => thread_rng().gen_range(15..=upper_bound),
+            PartySize::Precise(size) => size,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub enum Threshold {
+    Random,
+    Precise(usize),
+}
+
+impl Threshold {
+    pub fn get(self, party_size: usize) -> AnyResult<usize> {
+        match self {
+            Threshold::Random => Ok(thread_rng().gen_range(2..=party_size)),
+            Threshold::Precise(threshold) => {
+                if threshold > party_size {
+                    Err(MultisigError::ThresholdTooHigh.into())
+                } else {
+                    Ok(threshold)
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Action {
+    InitiateWithHash,
+    InitiateWithCall,
+    ApproveWithHash,
+    ApproveWithCall,
+    Cancel,
+}
+
+impl Action {
+    pub fn requires_call(&self) -> bool {
+        matches!(self, InitiateWithCall | ApproveWithCall)
+    }
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub enum Strategy {
+    Optimal,
+    Mess,
+    InAdvance,
+}
+
+use Action::*;
+use Strategy::*;
+
+impl Strategy {
+    fn initial_action(&self) -> Action {
+        match self {
+            Optimal => InitiateWithHash,
+            InAdvance => InitiateWithCall,
+            Mess if random() => InitiateWithHash,
+            _ => InitiateWithCall,
+        }
+    }
+
+    fn middle_action(&self) -> Action {
+        match self {
+            Optimal | InAdvance => ApproveWithHash,
+            Mess if random() => ApproveWithHash,
+            _ => ApproveWithCall,
+        }
+    }
+
+    fn final_action(&self, call_submitted: bool) -> Action {
+        match self {
+            Optimal => ApproveWithCall,
+            InAdvance => ApproveWithHash,
+            Mess if random() || !call_submitted => ApproveWithCall,
+            _ => ApproveWithHash,
+        }
+    }
+}
---
+++ scenarios/multisig/src/multisig.rs
@@ -0,0 +1,135 @@
+use std::time::Duration;
+
+use aleph_client::{
+    substrate_api_client::{extrinsic::balances::BalanceTransferXt, GenericAddress},
+    Connection, KeyPair, MultisigParty,
+};
+use anyhow::Result as AnyResult;
+use rand::{seq::index::sample, thread_rng, Rng};
+use serde::Deserialize;
+
+use crate::{Action, Cancel, PartySize, Strategy, Threshold};
+use chain_support::keypair_derived_from_seed;
+use common::{Ident, Scenario, ScenarioLogging};
+use scenarios_support::parse_interval;
+
+/// We operate on an account pool based on this seed. The final seeds will have
+/// a form of `MULTISIG_SEED{i: usize}`.
+const MULTISIG_SEED: &str = "//Multisig";
+
+/// We expect that there are as many endowed accounts (of seed phrases: `MULTISIG_SEED{i}`,
+/// where `i` is from 0 to this value (exclusively)).
+const AVAILABLE_ACCOUNTS: usize = 50;
+
+/// Returns keypair of the common account with index `idx`.
+fn compute_keypair(idx: usize) -> KeyPair {
+    keypair_derived_from_seed(format!("{}{}", MULTISIG_SEED, idx))
+}
+
+type Call = BalanceTransferXt;
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct MultisigConfig {
+    ident: Ident,
+    #[serde(deserialize_with = "parse_interval")]
+    interval: Duration,
+    party_size: PartySize,
+    threshold: Threshold,
+    strategy: Strategy,
+    cancel: bool,
+}
+
+#[derive(Clone)]
+pub struct Multisig {
+    ident: Ident,
+    interval: Duration,
+    party_size: usize,
+    threshold: usize,
+    strategy: Strategy,
+    cancel: bool,
+    connection: Connection,
+}
+
+impl Multisig {
+    pub fn new(connection: &Connection, config: MultisigConfig) -> Self {
+        let party_size = config.party_size.get(AVAILABLE_ACCOUNTS);
+        let threshold = config.threshold.get(party_size).unwrap();
+
+        Multisig {
+            ident: config.ident,
+            interval: config.interval,
+            party_size,
+            threshold,
+            strategy: config.strategy,
+            cancel: config.cancel,
+            connection: connection.clone(),
+        }
+    }
+
+    fn select_members(&self) -> Vec<KeyPair> {
+        let mut rng = thread_rng();
+        sample(&mut rng, AVAILABLE_ACCOUNTS, self.party_size)
+            .iter()
+            .map(compute_keypair)
+            .collect()
+    }
+
+    fn prepare_actions(&self) -> Vec<Action> {
+        let mut actions = vec![self.strategy.initial_action()];
+        let mut call_submitted = actions[0].requires_call();
+
+        for _ in 1..self.threshold {
+            let next_action = self.strategy.middle_action();
+            call_submitted |= next_action.requires_call();
+            actions.push(next_action)
+        }
+        actions.push(self.strategy.final_action(call_submitted));
+
+        if self.cancel {
+            let i = thread_rng().gen_range(1..=self.threshold);
+            actions[i] = Cancel;
+        }
+
+        actions
+    }
+
+    fn prepare_call(&self) -> Call {
+        self.connection
+            .balance_transfer(GenericAddress::Address32(Default::default()), 0)
+    }
+
+    async fn perform_multisig(
+        &self,
+        members: Vec<KeyPair>,
+        actions: Vec<Action>,
+        call: Call,
+    ) -> AnyResult<()> {
+        let party = MultisigParty::new(members.clone(), self.threshold as u16);
+
+        Ok(())
+    }
+}
+
+#[async_trait::async_trait]
+impl Scenario for Multisig {
+    fn interval(&self) -> Duration {
+        self.interval
+    }
+
+    async fn play(&mut self) -> AnyResult<()> {
+        self.info("Starting scenario");
+
+        let members = self.select_members();
+        let actions = self.prepare_actions();
+        let call = self.prepare_call();
+
+        self.perform_multisig(members, actions, call).await?;
+
+        self.info("Scenario finished successfully");
+        Ok(())
+    }
+
+    fn ident(&self) -> Ident {
+        self.ident.clone()
+    }
+}
