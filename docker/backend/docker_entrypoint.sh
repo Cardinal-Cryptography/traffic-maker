ca3b8f2d501817d1dffacd44a004b3b07352d099
Piotr Mikołajczyk <piomiko41@gmail.com>
2022-06-15 15:43:52 +0200

    A0-1018: docker compose (#40)

--- docker/backend/docker_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-STATS_BASE_URL=${STATS_BASE_URL:-"http://127.0.0.1:8080"}
-
-RUST_LOG=warn backend
--- monitoring/src/main.rs
+++ monitoring/src/main.rs
@@ -7,7 +7,7 @@ mod data;
 mod message;
 mod view;
 
-pub const DEFAULT_STATS_BASE_URL: &str = "http://127.0.0.1:8080";
+pub const DEFAULT_STATS_BASE_URL: &str = "http://localhost:8080";
 
 pub fn main() -> Result {
     // We need to use macro to resolve this in compilation time, as during execution
